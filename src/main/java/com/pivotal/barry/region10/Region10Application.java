6915e8d4ff105d8e61345355d7c8de7ac497db30
bmullan-pivotal <bmullan@pivotal.io>
2019-07-11 14:52:41 -0400

    updates

--- src/main/java/com/pivotal/barry/region10/Region10Application.java
+++ src/main/java/com/pivotal/barry/region10/Region10Application.java
@@ -1,23 +1,21 @@
 package com.pivotal.barry.region10;
 
-import org.apache.geode.cache.Cache;
-import org.apache.geode.cache.CacheFactory;
-import org.apache.geode.pdx.ReflectionBasedAutoSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
 import org.springframework.data.gemfire.config.annotation.EnableClusterConfiguration;
 import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
-import org.springframework.data.gemfire.config.annotation.EnablePdx;
-import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
+import org.springframework.data.gemfire.function.annotation.GemfireFunction;
+import org.springframework.data.gemfire.function.config.EnableGemfireFunctionExecutions;
 
 @SpringBootApplication
 @EnableEntityDefinedRegions(basePackageClasses = {Region10.class,CompoundKey.class})
 @EnableClusterConfiguration(useHttp = true )
+@EnableGemfireFunctionExecutions
 public class Region10Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Region10Application.class, args);
 	}
-
+	
 }
+
